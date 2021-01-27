c9d47ec028f0c31ae6614ea96c30a2e4f0ba9f37
hrithik_bt18 <hrithik_bt18@iiitkalyani.ac.in>
2021-01-27 15:50:59 +0530

    made some changes

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-  user: "postgres",
-  password: "Hrithik@143",
-  host: "localhost",
+  user: "ucayscuisetczf",
+  password: "88ca7a5c2fc58bf521b995772875d9f8a3d00b335e4a4a32d146c2e13bd298e4",
+  host: `ec2-52-72-190-41.compute-1.amazonaws.com`,
   port: 5432,
-  database: "perntodo",
+  database: "dcqosjf26g2gvm",
 });
 
 module.exports = pool;
