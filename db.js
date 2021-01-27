583fe21869d1716a055917d7803cedab2075386e
hrithik_bt18 <hrithik_bt18@iiitkalyani.ac.in>
2021-01-27 15:57:10 +0530

    added the host

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const Pool = require("pg").Pool;
 const pool = new Pool({
   user: "ucayscuisetczf",
   password: "88ca7a5c2fc58bf521b995772875d9f8a3d00b335e4a4a32d146c2e13bd298e4",
-  host: `ec2-52-72-190-41.compute-1.amazonaws.com`,
+  host: "https://pern-todo-hrithik.herokuapp.com/",
   port: 5432,
   database: "dcqosjf26g2gvm",
 });
