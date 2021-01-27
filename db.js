ac972f77c980dc43006a2b753902a71a25a7d295
hrithik_bt18 <hrithik_bt18@iiitkalyani.ac.in>
2021-01-27 17:08:11 +0530

    changes in db

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-  user: "ucayscuisetczf",
-  password: "88ca7a5c2fc58bf521b995772875d9f8a3d00b335e4a4a32d146c2e13bd298e4",
-  host: "https://pern-todo-hrithik.herokuapp.com/",
+  user: process.env.User || 'postgres',
+  password: process.env.Password || 'Hrithik@143',
+  host: process.env.Host || 'localhost',
   port: 5432,
-  database: "dcqosjf26g2gvm",
+  database: process.env.Database || 'perntodo',
 });
 
 module.exports = pool;
